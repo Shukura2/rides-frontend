8737e78cae0aecb30557347bc570a040e2f3af70
Shukurah <shukkike@gmail.com>
2023-01-17 11:34:46 +0100

    Other pages

---
+++ pages/products.tsx
@@ -0,0 +1,75 @@
+import { Lexend } from "@next/font/google";
+import Box from "@mui/material/Box";
+import MainLayout from "@/components/layouts/MainLayout";
+
+const lexend = Lexend({ subsets: ["latin"] });
+
+const OurProducts = (): JSX.Element => {
+  return (
+    <Box sx={{ maxWidth: "1536px", margin: "0 auto" }}>
+      <Box
+        sx={{ padding: { xs: "30px 20px", md: "50px" }, textAlign: "justify" }}
+        className={lexend.className}
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, eum
+        dolorum tempora veniam maxime omnis fugiat quod facilis quos ducimus
+        inventore itaque iure rem totam ipsum temporibus nam dolore nesciunt.
+        Iusto modi ab fuga delectus sapiente aperiam eum, quo rerum excepturi
+        eaque, libero quibusdam ad quia cupiditate. Reiciendis tenetur eligendi
+        quidem tempora optio adipisci molestiae natus iusto vel non eveniet
+        aperiam vitae nostrum ea placeat sit ratione consequuntur dolorem, est
+        officiis autem, quos fuga asperiores. Fugit illum possimus modi dolores
+        aperiam autem assumenda voluptas sint aliquam necessitatibus
+        exercitationem nesciunt asperiores delectus obcaecati ipsum mollitia
+        quaerat minus architecto, dolorem rem tempore quos officia neque! Itaque
+        eaque earum quod nam eius, modi iure, quos nisi harum vel unde
+        distinctio reprehenderit commodi ullam accusantium neque veritatis
+        aspernatur voluptatum ipsum nesciunt quae? Inventore explicabo modi,
+        nobis cum, dolorem dolor necessitatibus possimus aut itaque molestias
+        consequuntur non at reprehenderit repudiandae magnam perferendis id
+        excepturi sunt optio facere iusto voluptate. Ab, consequuntur! Nulla
+        ullam incidunt optio sequi. Ullam, natus. Nemo ipsa suscipit voluptates
+        consequatur reprehenderit, sit accusamus quae! Eum molestias ipsam
+        repellat est perferendis? Quidem voluptates odit sequi ipsam iste
+        molestias illum voluptate commodi in eius, corrupti suscipit quos
+        tenetur ratione qui doloremque consequatur cum laborum, sed aperiam,
+        possimus fugiat dolorem nesciunt. Ipsam soluta adipisci beatae voluptas
+        reprehenderit illum unde voluptatem quae ducimus atque, numquam,
+        incidunt cumque a facilis dolorem esse quibusdam neque dolorum. Quam
+        laborum culpa possimus maxime facere magnam provident nemo sit impedit
+        nesciunt, tempore omnis laboriosam suscipit voluptatem nihil neque
+        aliquam, accusantium soluta voluptas. Quaerat nihil, aliquid dolorem
+        impedit tempore iste consequuntur, temporibus ea pariatur itaque maiores
+        dolor dignissimos ipsam quos provident, sit doloribus. Aspernatur,
+        animi! Aspernatur architecto qui obcaecati ducimus ut quos! Consectetur,
+        molestiae velit debitis fugiat quo rerum vero nisi vel, soluta atque
+        quis dolores reprehenderit minima, eligendi quae sed! Sunt illum nulla
+        esse voluptatum, doloribus laboriosam perferendis tempore molestiae quia
+        fugiat, dicta nesciunt odit voluptas optio et unde? Aliquid, voluptatum
+        accusantium facere optio numquam similique eveniet porro non temporibus
+        officiis assumenda, veritatis est tempora velit minus dolorem laborum
+        pariatur corrupti? Numquam id consequuntur perferendis sapiente culpa
+        laborum voluptate placeat est nobis ipsum illo adipisci eos architecto
+        non nulla cum molestiae officia a expedita pariatur, odio labore ipsa
+        laudantium earum. Est, nobis officiis! Optio harum, vel commodi cumque,
+        excepturi nesciunt voluptates suscipit fugiat ipsum nam sint esse
+        repellat molestias asperiores inventore, voluptatibus deserunt in nulla
+        explicabo laborum ducimus? Dolore placeat magnam soluta sunt aliquam,
+        nulla adipisci expedita vel fugit sint praesentium doloremque aliquid
+        exercitationem numquam sapiente alias perspiciatis sed ex eius labore
+        temporibus? Reiciendis ab aspernatur voluptatum eligendi consequatur
+        animi ad fugit est, officiis tenetur placeat esse quo illum voluptate!
+        Pariatur dignissimos aliquid explicabo earum repudiandae rem illo atque
+        consectetur esse quae. Sunt voluptatem eaque incidunt labore soluta nemo
+        amet tempora debitis est ipsam dicta, laudantium corrupti, dolorem eius
+        distinctio tenetur necessitatibus? Aspernatur quisquam amet ullam,
+        voluptates asperiores consectetur est reiciendis eveniet voluptatum
+        expedita totam quas iste repellendus aperiam obcaecati ut distinctio rem
+        dolorem cum qui. Iusto illo corrupti animi quidem.
+      </Box>
+    </Box>
+  );
+};
+
+OurProducts.getLayout = MainLayout;
+export default OurProducts;
